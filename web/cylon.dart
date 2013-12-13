325a34e3f4ee8330a93378166e54af3254727017
Noah Koch <noahtkoch@gmail.com>
2013-12-13 16:55:01 -0600

    "Div" not explicitly required

--- web/cylon.dart
+++ web/cylon.dart
@@ -28,17 +28,23 @@ class ParseDocument{
   }
   
   getElement(String node){
+//    Evaluates only the attributes of the specific node
     List elements = node.split('/');
     List validElements = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdi","bdo","bgsound","big","blink","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup","content","data","datalist","dd","decorator","del","details","dfn","dir","div","dl","dt","em","embed","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","i","iframe","img","input","ins","isindex","kbd","keygen","label","legend","li","link","listing","main","map","mark","marquee","menu","menuitem","meta","meter","nav","nobr","noframes","noscript","object","ol","optgroup","option","output","p","param","plaintext","pre","progress","q","rp","rt","ruby","s","samp","section","select","shadow","small","source","spacer","span","strike","strong","sub","summary","sup","table","tbody","td","template","textarea","tfoot","th","thead","time","tr","track","tt","u","ul","var","video","wbr","xmp"];
     
     if(elements.length ==  1){
       return ["div",""];
     }else{
+//      If it's not a div figure out what it is. 
       List elementTypes = elements[0].split(' ');
       int matchingElementIndex = validElements.indexOf(elementTypes[0]);
-      String element = validElements[matchingElementIndex];
-      elementTypes.removeAt(0);
-      return [element,elementTypes.join(' ')];
+      if(matchingElementIndex > 0){
+        String element = validElements[matchingElementIndex];
+        elementTypes.removeAt(0);
+        return [element,elementTypes.join(' ')];
+      }else{
+        return ["div", elementTypes.join(' ')];      
+      }
     }
   }
   
