08e88f2c87fd061d318d6329360c35d195bd2b1a
Noah Koch <noahtkoch@gmail.com>
2013-12-13 16:49:03 -0600

    Basic functionality

--- web/cylon.dart
+++ web/cylon.dart
@@ -1,5 +1,4 @@
 import 'dart:html';
-//import 'dart:convert';
 
 void main() {
   new ParseDocument(bodyEl: querySelector('.cylon'));
@@ -21,21 +20,34 @@ class ParseDocument{
     _endingTags = new List();
     var openDivs = _original.split('[');
     openDivs.forEach((div){
-      String elTag = getElement(div.toString());
-      _startingTags.add('<' + elTag + '>');
-      _endingTags.add('</' + elTag + '>');
+      List elTag = getElement(div);
+      _startingTags.add('<' + elTag[0] + ' ' + elTag[1] + '>');
+      _endingTags.add('</' + elTag[0] + '>');
+      
     });
   }
   
   getElement(String node){
-    return "div";
+    List elements = node.split('/');
+    List validElements = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdi","bdo","bgsound","big","blink","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup","content","data","datalist","dd","decorator","del","details","dfn","dir","div","dl","dt","em","embed","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","i","iframe","img","input","ins","isindex","kbd","keygen","label","legend","li","link","listing","main","map","mark","marquee","menu","menuitem","meta","meter","nav","nobr","noframes","noscript","object","ol","optgroup","option","output","p","param","plaintext","pre","progress","q","rp","rt","ruby","s","samp","section","select","shadow","small","source","spacer","span","strike","strong","sub","summary","sup","table","tbody","td","template","textarea","tfoot","th","thead","time","tr","track","tt","u","ul","var","video","wbr","xmp"];
+    
+    if(elements.length ==  1){
+      return ["div",""];
+    }else{
+      List elementTypes = elements[0].split(' ');
+      int matchingElementIndex = validElements.indexOf(elementTypes[0]);
+      String element = validElements[matchingElementIndex];
+      elementTypes.removeAt(0);
+      return [element,elementTypes.join(' ')];
+    }
   }
   
   generateHTML(){
     var openDivs = _original.split('[');
     List generated = new List();
     for(var i=0;i<openDivs.length;i++){
-      generated.add(_startingTags[i] + countEnding(openDivs[i]));      
+      String stripTags = openDivs[i].split('/').length > 1 ? openDivs[i].split('/')[1] : openDivs[i];
+      generated.add(_startingTags[i] + countEnding(stripTags));      
     };
     return generated.join();
   }
