29c190209f529fa3958fe6b793753b61d27d53ca
Noah Koch <noahtkoch@gmail.com>
2013-12-20 14:25:44 -0600

    gitignore

--- web/cylon.dart
+++ web/cylon.dart
@@ -1,7 +1,12 @@
 import 'dart:html';
+import 'package:routes.dart';
+
+library client;
 
 void main() {
   new ParseDocument(bodyEl: querySelector('.cylon'));
+  var router = new Router()
+    ..addHandler(pageURL,parsePage);
 }
 
 class ParseDocument{
